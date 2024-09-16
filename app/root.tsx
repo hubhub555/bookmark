915fe7859136684d8962569eea341a8b68166be3
hubhub555 <zbuhibuhi@gmail.com>
2024-09-16 23:27:59 +0900

    Fix：スタイルが効かないバグ修正

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Outlet, Scripts, ScrollRestoration, Links } from "@remix-run/react";
 import { Header } from "./components/header";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
@@ -9,12 +9,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links />
       </head>
       <body style={{ backgroundColor: "gray" }}>
         <MantineProvider>
+          <Header />
           <ScrollRestoration />
           <Scripts />
-          <Header />
           {children}
         </MantineProvider>
       </body>
